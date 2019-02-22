1d3f1c271a9b4f2376de4983e2d3b2800cbb8aa0
joe <joe@sixmops.com>
2019-02-22 14:02:50 -0500

    updated module info

--- HelloImagination/imagination/component/src/main/java/module-info.java
+++ HelloImagination/imagination/component/src/main/java/module-info.java
@@ -1,8 +1,6 @@
 module com.sixmops.component.bits {
 	requires transitive java.json;
-	//requires transitive org.glassfish.java.json;
 	requires java.base;
 	requires java.net.http;
-    //requires transitive javafx.controls;
 	exports com.sixmops.component;
 }
